bb453337f29d9750a324ef33fd4f5b32a7f8c412
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2014-06-17 10:31:42 -0700

    Puppet agent start script works as expected.

--- scripts/puppet-agent.sh
+++ scripts/puppet-agent.sh
@@ -3,22 +3,15 @@
 # load environment variables
 source /etc/container_environment.sh
 
-function write_puppet_config () {
-    read -d '' puppet_config <<EOF
-[agent]
-server=$1
-masterport=$2
-EOF
-    
-    echo -e "$puppet_config" >> /etc/puppet/puppet.conf
-}
-
 # default puppet master port is 8410
-test -z "$PUPPET_MASTER_TCP_PORT" && export PUPPET_MASTER_TCP_PORT="8410"
+test -z "$PUPPETMASTER_TCP_PORT" && export PUPPETMASTER_TCP_PORT="8410"
 
-# if there is a puppet master host defined, rewrite the config to match
+puppet_agent_args="--no-daemonize"
+
+# if there is a puppet master host defined, append the server and port parameters
 if [ ! -z "$PUPPET_MASTER_TCP_HOST" ]; then 
-    write_puppet_config "$PUPPET_MASTER_TCP_HOST" "$PUPPET_MASTER_TCP_PORT"
+    puppet_agent_args="$puppet_agent_args --server \"$PUPPETMASTER_TCP_HOST\" --masterport $PUPPETMASTER_TCP_PORT"
 fi
 
-exec puppet agent --no-daemonize
+# start the puppet agent in foreground with given arguments
+exec puppet agent $puppet_agent_args

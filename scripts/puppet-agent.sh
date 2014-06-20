caf98f96beb4beba5f75b888cf67323deb7caa64
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2014-06-19 17:42:06 -0700

    Bug fix for Puppet Agent: not appending server name when present.

--- scripts/puppet-agent.sh
+++ scripts/puppet-agent.sh
@@ -9,8 +9,8 @@ test -z "$PUPPETMASTER_TCP_PORT" && export PUPPETMASTER_TCP_PORT="8410"
 puppet_agent_args="--no-daemonize"
 
 # if there is a puppet master host defined, append the server and port parameters
-if [ ! -z "$PUPPET_MASTER_TCP_HOST" ]; then 
-    puppet_agent_args="$puppet_agent_args --server \"$PUPPETMASTER_TCP_HOST\" --masterport $PUPPETMASTER_TCP_PORT"
+if [ ! -z "$PUPPETMASTER_TCP_HOST" ]; then 
+    puppet_agent_args="$puppet_agent_args --server $PUPPETMASTER_TCP_HOST --masterport $PUPPETMASTER_TCP_PORT"
 fi
 
 # start the puppet agent in foreground with given arguments
